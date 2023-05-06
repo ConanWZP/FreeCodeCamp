22eed2aa1dbaf8336e43b02643a0e746ecdde149
ConanWZP <yaroslav2281337@mail.ru>
2023-05-06 17:20:33 +0300

    Word Blanks

---
+++ Basic JavaScript/WordBlanks.js
@@ -0,0 +1,9 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "A " + myAdjective + " " + myNoun + " " + myVerb + " really " + myAdverb + '.';  // Change this line
+// Only change code above this line
+console.log(wordBlanks) //A big dog ran really quickly.
