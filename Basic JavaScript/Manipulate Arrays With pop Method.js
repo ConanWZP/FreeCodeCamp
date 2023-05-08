53689e0149b2a9ab1cc6fb5d49fef30399067f2f
ConanWZP <yaroslav2281337@mail.ru>
2023-05-08 12:11:26 +0300

    Manipulate Arrays With pop Method

---
+++ Basic JavaScript/Manipulate Arrays With pop Method.js
@@ -0,0 +1,7 @@
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+const removedFromMyArray = myArray.pop();  // <--
+// Only change code below this line
+
+console.log(myArray)  //  [["John", 23]] т.е. удалли последний элемент из myArray
+console.log(removedFromMyArray) // [ 'cat', 2 ] // сохранили последний элемент из myArray
