418755ec83affb4a22b0b25b290c96266305a5de
ConanWZP <yaroslav2281337@mail.ru>
2023-05-14 12:55:59 +0300

    Profile Lookup

--- Basic JavaScript/Profile Lookup.js
+++ Basic JavaScript/Profile Lookup.js
@@ -28,8 +28,38 @@ const contacts = [
 
 function lookUpProfile(name, prop) {
     // Only change code below this line
+   /* for (let i = 0; i < contacts.length; i++) {
+        if (contacts[i].firstName === name) {
+            if (contacts[i].hasOwnProperty(prop)) {
+                return contacts[i][prop]
+            } else {
+                return `No such property`
+            }
+
+        } else {
+            return `No such contact`
+        }
+
+    }*/
+
+
+    let a = contacts.filter((cont) => cont.firstName === name)
+
+    if (a.length === 0) {
+        return 'No such contact'
+    } else {
+        if (a[0].hasOwnProperty(prop)) {
+            return a[0][prop]
+        } else {
+            return 'No such property'
+        }
+    }
+
+
+
 
     // Only change code above this line
 }
 
-lookUpProfile("Akira", "likes");
+
+console.log(  lookUpProfile("Kristian", "lastName")  )
