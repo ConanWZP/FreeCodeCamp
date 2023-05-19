a8f329a46f65581a56059d5419bc6d33dd504ecc
ConanWZP <yaroslav2281337@mail.ru>
2023-05-19 12:09:41 +0300

    Match Single Characters Not Specified

---
+++ Regular Expressions/Match Single Characters Not Specified.js
@@ -0,0 +1,6 @@
+let quoteSample = "3 blind mice.";
+let myRegex = /[^aeiou0-9]/gi; // Change this line
+let result = quoteSample.match(myRegex); // Change this line
+
+
+console.log( result.length  ) // 9
