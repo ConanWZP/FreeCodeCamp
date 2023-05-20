28d63ad1936586bfdbaf3a1bc95493d02e92f6a8
ConanWZP <yaroslav2281337@mail.ru>
2023-05-20 12:13:08 +0300

    Specify Only the Lower Number of Matches

---
+++ Regular Expressions/Specify Only the Lower Number of Matches.js
@@ -0,0 +1,6 @@
+let haStr = "Hazzzzah";
+let haRegex = /haz{4,}ah/i; // Change this line
+let result = haRegex.test(haStr);
+
+//  Change the regex haRegex to match the word Hazzah only when it has four or more letter z's.
+
