db849617dd7390d5f56fb1beaedbd4a0a9a7e2c3
ConanWZP <yaroslav2281337@mail.ru>
2023-05-20 14:27:39 +0300

    Check for All or None

---
+++ Regular Expressions/Check for All or None.js
@@ -0,0 +1,12 @@
+let favWord = "favorite";
+let favRegex = /favou?rite/; // Change this line
+let result = favRegex.test(favWord);
+
+console.log(  result  ) // true и для  favWord = "favourite" тоже true
+
+/*
+
+
+
+
+*/
