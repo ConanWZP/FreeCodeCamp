820279a75630529265705810cc1ed527be12b12a
ConanWZP <yaroslav2281337@mail.ru>
2023-05-24 12:22:10 +0300

    Add Items Using splice()

---
+++ Basic Data Structures/Add Items Using splice().js
@@ -0,0 +1,8 @@
+function htmlColorNames(arr) {
+    // Only change code below this line
+    arr.splice(0, 5, "DarkSalmon", "BlanchedAlmond", "LavenderBlush", "PaleTurquoise", "FireBrick")
+    // Only change code above this line
+    return arr;
+}
+
+console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
