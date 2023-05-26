d2bfa6a645226c5bc2ef4bc6a0657160d0aded04
ConanWZP <yaroslav2281337@mail.ru>
2023-05-26 15:34:32 +0300

    Create complex multi-dimensional arrays

---
+++ Basic Data Structures/Create complex multi-dimensional arrays.js
@@ -0,0 +1,32 @@
+let myNestedArray = [
+    // Only change code below this line
+                    [
+                        [
+                            'loop',
+                            'deep',
+                             6,
+                            7,
+                            1000,
+                            [
+                                'deeper',
+                                false,
+                                true,
+                                [
+                                    'deepest',
+                                    1327.98,
+                                    'splice',
+                                    'push'
+                                ],
+                                'array']
+                        ],
+                        false,
+                        1,
+                        2,
+                        3,
+                        'complex',
+                        'nested'
+                    ],
+
+
+    // Only change code above this line
+];
