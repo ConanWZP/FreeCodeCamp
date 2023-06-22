0e3fb4f385965f4fbe6ab0f8304a8e7f13e89d55
ConanWZP <yaroslav2281337@mail.ru>
2023-06-22 20:26:31 +0300

    Sum All Primes

---
+++ Intermediate Algorithm Scripting/Sum All Primes.js
@@ -0,0 +1,25 @@
+function sumPrimes(num) {
+
+    let sum = 0
+
+        for (let i = 2; i <= num; i++) {
+            if (isPrime(i)) {
+                sum += i
+            }
+        }
+
+
+
+    return sum
+}
+
+function isPrime(n) {
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) {
+            return false
+        }
+    }
+    return  true
+}
+
+console.log(    sumPrimes(10)      )
